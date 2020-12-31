e6e354621cd64d6d92c427dd2c7a460a5db5eaa0
tingtingtan <54339802+tingtingtan@users.noreply.github.com>
2020-12-30 22:03:25 -0600

    width adjusted

---
+++ bar.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });
+
+
+  $(window).resize(function() {
+	   if (  $(window).width() > 1130 ) {
+		$('nav ul').removeAttr('style');
+	 }
+	});
+
+  $(window).resize(function() {
+     if (  $(window).width() < 1130 ) {
+    $('nav ul').removeAttr('style');
+   }
+  });
+});
--- menu.js
+++ menu.js
@@ -1,24 +1,3 @@
-
-$(document).ready(function() {
-  $('.menu-trigger').click(function() {
-    $('nav ul').slideToggle(500);
-  });
-
-
-  $(window).resize(function() {
-	   if (  $(window).width() > 1130 ) {
-		$('nav ul').removeAttr('style');
-	 }
-	});
-
-  $(window).resize(function() {
-     if (  $(window).width() < 1130 ) {
-    $('nav ul').removeAttr('style');
-   }
-  });
-});
-
-
 var words = document.getElementsByClassName('word');
 var wordArray = [];
 var currentWord = 0;
